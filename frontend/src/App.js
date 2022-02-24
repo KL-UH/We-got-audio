c4c6fb2336c03479401d208def108b1a36251983
Haneesh-jpeg <malepatihaneesh@gmail.com>
2022-02-24 09:01:10 +0530

    Added type-box for url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
+import InputBar from './Components/InputBar/InputBar';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <InputBar />
     </div>
   );
 }
--- frontend/src/Components/InputBar/InputBar.js
+++ frontend/src/Components/InputBar/InputBar.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react';
+import { Button, Form } from 'react-bootstrap';
+import './InputBar.css';
 
 function InputBar() {
+
+  const [todo, setTodo] = useState("");
+  const [list, setList] = useState([]);
+
+  const addTodo = (e) => {
+    if (!todo) {
+      alert("Enter Todo");
+    }
+    else {
+      e.preventDefault();
+      setList(...list, todo);
+      setTodo("");
+    }
+  };
+
+
   return (
     <>
-        
+      <div className='Search'>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Control type="text" placeholder="Paste Video link" value={todo} onChange={(e) => setTodo(e.target.value)} />
+          </Form.Group>
+
+          <Button className="button" variant="primary" type="submit" onClick={addTodo}>
+            Submit
+          </Button>
+        </Form>
+      </div>
     </>
   )
 }
