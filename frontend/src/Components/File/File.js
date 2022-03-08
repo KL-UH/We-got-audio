ddc4849be6d7f04435d2ef3a1e6fc3a8860a9751
Haneesh-jpeg <malepatihaneesh@gmail.com>
2022-03-08 22:29:07 +0530

    added File page

--- frontend/src/Components/File/File.js
+++ frontend/src/Components/File/File.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import { Outlet, Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 function File() {
   return (
     <div className='File'>
         <NavBar />
+
+        {/* Navigate to URL and File */}
+      <div className='toggle'>
+        <li>
+          <Link className='LinkTag' to='/'>
+            <Button>URL</Button>
+          </Link>
+        </li>
+        <li>
+          <Link className='LinkTag' to='/file'>
+            <Button>File</Button>
+          </Link>
+        </li>
+      </div>
+
+
+      <Outlet />
     </div>
   )
 }
